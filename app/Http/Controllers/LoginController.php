f1cbc65072a00403b7b85dadf09390bf4c474e25
unknown <adven.setiawan7@gmail.com>
2018-07-23 01:34:00 +0700

    selesai recomended

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -15,20 +15,10 @@ class LoginController extends Controller
         if($check) {
             $user = User::where('email', $request->email)->first();
 
-<<<<<<< HEAD
-       if($user->is_admin())
-       {
-         return redirect()->route('dashboard');
-       }
-       return redirect()->route('index');
-     }
-     return redirect()->back();
-=======
             return redirect()->to($user->redirectUrlAfterLogin());
         }
         return redirect()->back();
         
->>>>>>> 7f3a4e96b7375ee845cacd885cc27fe308dffbb1
    }
 
    public function __construct()
