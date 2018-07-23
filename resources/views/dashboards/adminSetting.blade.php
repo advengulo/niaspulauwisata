50e5dc28741045eb51cf36571abb8a3387854870
unknown <adven.setiawan7@gmail.com>
2018-07-24 00:04:31 +0700

    admin setting

--- resources/views/dashboards/adminSetting.blade.php
+++ resources/views/dashboards/adminSetting.blade.php
@@ -21,23 +21,22 @@
                         <div class="row mtl">
                             <div class="col-md-3">
                                 <div class="form-group">
-                                <div class="text-center mbl"><img src="{{asset('')}}img/team-1.jpg" alt="" class="img-responsive"/></div>
+                                @foreach($dataAdmin as $data)
+                                @if($data->admin==true)
+                                <div class="text-center mbl"><img src="{{$data->avatar}}" alt="" class="img-responsive"/></div>
                                     <div class="text-center mbl"><a href="#" class="btn btn-green"><i class="fa fa-upload"></i>&nbsp;
                                         Upload</a></div>
                                 </div>
                                 <table class="table table-striped table-hover">
+                                    
                                     <tbody>
                                     <tr>
                                         <td>User Name</td>
-                                        <td>John Doe</td>
+                                        <td>{{$data->name}}</td>
                                     </tr>
                                     <tr>
                                         <td>Email</td>
-                                        <td>name@example.com</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Address</td>
-                                        <td>Street 123, Avenue 45, Country</td>
+                                        <td>{{$data->email}}</td>
                                     </tr>
                                     <tr>
                                         <td>Status</td>
@@ -46,10 +45,6 @@
                                     <tr>
                                         <td>User Rating</td>
                                         <td><i class="fa fa-star text-yellow fa-fw"></i><i class="fa fa-star text-yellow fa-fw"></i><i class="fa fa-star text-yellow fa-fw"></i><i class="fa fa-star text-yellow fa-fw"></i><i class="fa fa-star text-yellow fa-fw"></i></td>
-                                    </tr>
-                                    <tr>
-                                        <td>Member Since</td>
-                                        <td> Jun 03, 2014</td>
                                     </tr>
                                     </tbody>
                                 </table>
@@ -66,7 +61,7 @@
 
                                                 <div class="col-sm-9 controls">
                                                     <div class="row">
-                                                        <div class="col-xs-9"><input type="email" placeholder="email@yourcompany.com" class="form-control"/></div>
+                                                        <div class="col-xs-9"><input type="email" value={{$data->email}}  class="form-control"/></div>
                                                     </div>
                                                 </div>
                                             </div>
@@ -74,7 +69,7 @@
 
                                                 <div class="col-sm-9 controls">
                                                     <div class="row">
-                                                        <div class="col-xs-9"><input type="text" placeholder="username" class="form-control"/></div>
+                                                        <div class="col-xs-9"><input type="text" value={{$data->name}} class="form-control"/></div>
                                                     </div>
                                                 </div>
                                             </div>
@@ -94,113 +89,8 @@
                                                     </div>
                                                 </div>
                                             </div>
-                                            <hr/>
-                                            <h3>Profile Setting</h3>
-
-                                            <div class="form-group"><label class="col-sm-3 control-label">First Name</label>
-
-                                                <div class="col-sm-9 controls">
-                                                    <div class="row">
-                                                        <div class="col-xs-9"><input type="text" placeholder="first name" class="form-control"/></div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="form-group"><label class="col-sm-3 control-label">Last Name</label>
-
-                                                <div class="col-sm-9 controls">
-                                                    <div class="row">
-                                                        <div class="col-xs-9"><input type="text" placeholder="last name" class="form-control"/></div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="form-group"><label class="col-sm-3 control-label">Gender</label>
-
-                                                <div class="col-sm-9 controls">
-                                                    <div class="row">
-                                                        <div class="col-xs-9">
-                                                            <div class="radio"><label class="radio-inline"><input type="radio" value="0" name="gender" checked="checked"/>&nbsp;
-                                                                Male</label><label class="radio-inline"><input type="radio" value="1" name="gender"/>&nbsp;
-                                                                Female</label></div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="form-group"><label class="col-sm-3 control-label">Birthday</label>
-
-                                                <div class="col-sm-9 controls">
-                                                    <div class="row">
-                                                        <div class="col-xs-4"><input id="datepicker-normal" type="text" class="form-control"/></div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="form-group"><label class="col-sm-3 control-label">Marital Status</label>
-
-                                                <div class="col-sm-9 controls">
-                                                    <div class="row">
-                                                        <div class="col-xs-4"><select class="form-control">
-                                                            <option>Single</option>
-                                                            <option>Married</option>
-                                                        </select></div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="form-group"><label class="col-sm-3 control-label">Position</label>
-
-                                                <div class="col-sm-9 controls">
-                                                    <div class="row">
-                                                        <div class="col-xs-4"><select class="form-control">
-                                                            <option>CEO</option>
-                                                            <option>Director</option>
-                                                            <option>Manager</option>
-                                                            <option>Staff</option>
-                                                            <option>Office Boy</option>
-                                                        </select></div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="form-group"><label class="col-sm-3 control-label">About</label>
-
-                                                <div class="col-sm-9 controls">
-                                                    <div class="row">
-                                                        <div class="col-xs-9"><textarea rows="3" class="form-control"></textarea></div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <hr/>
-                                            <h3>Contact Setting</h3>
-
-                                            <div class="form-group"><label class="col-sm-3 control-label">Mobile Phone</label>
-
-                                                <div class="col-sm-9 controls">
-                                                    <div class="row">
-                                                        <div class="col-xs-9"><input type="text" placeholder="mobile phone" class="form-control"/></div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="form-group"><label class="col-sm-3 control-label">Website</label>
-
-                                                <div class="col-sm-9 controls">
-                                                    <div class="row">
-                                                        <div class="col-xs-9"><input type="text" placeholder="website" class="form-control"/></div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="form-group"><label class="col-sm-3 control-label">Facebook</label>
-
-                                                <div class="col-sm-9 controls">
-                                                    <div class="row">
-                                                        <div class="col-xs-9"><input type="text" placeholder="facebook" class="form-control"/></div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="form-group"><label class="col-sm-3 control-label">Twitter</label>
-
-                                                <div class="col-sm-9 controls">
-                                                    <div class="row">
-                                                        <div class="col-xs-9"><input type="text" placeholder="twitter" class="form-control"/></div>
-                                                    </div>
-                                                </div>
-                                            </div>
+                                            @endif
+                                            @endforeach
                                             <hr/>
                                             <button type="submit" class="btn btn-green btn-block">Finish</button>
                                         </form>
